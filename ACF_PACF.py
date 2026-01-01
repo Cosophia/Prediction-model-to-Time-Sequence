990bba5c6f3c8ec415de96d62e3d43c1dd039d22
Cosophia <57757955@qq.com>
2026-01-01 16:21:51 +0800

    Updating some code and its Note.

--- ACF_PACF.py
+++ ACF_PACF.py
@@ -8,7 +8,7 @@ from statsmodels.graphics.tsaplots import plot_acf,plot_pacf
 
 plt.rcParams['font.family'] = 'SimHei' # 使用黑体
 plt.rcParams['axes.unicode_minus'] = False # 正常显示负号
-'''这里是通过调用看ACF、PACF的趋势'''
+'''这里是通过调用展示ACF、PACF的趋势'''
 # # 预分配
 # lags = [1,2,3]
 # data = np.array(np.random.randint(-10,40,size=80))
@@ -31,6 +31,12 @@ plt.rcParams['axes.unicode_minus'] = False # 正常显示负号
 
 '''手动计算ACF和PACF,这里仅仅以lags = [1,2,3]中为例子'''
 def ACF_calculation(data_ori,data_t,data_lags):
+    """
+    :param data_ori: 原序列
+    :param data_t: 当前时刻序列
+    :param data_lags: 滞后序列
+    :return: ACF数值
+    """
     cov_data = np.cov(data_t,data_lags)[0,1]
     var_data = np.var(data_ori,ddof=1)
     return cov_data/var_data
@@ -39,22 +45,34 @@ def PACF_calculation():
     pass
 
 if __name__ == "__main__":
+    # 滞后数列表
     lags = [1,2,3]
-    data = np.array([12, 37, 8, 25, 41,45,37,28])
     # 原序列
+    data = np.array([12, 37, 8, 25, 41,45,37,28])
     for flag in range(len(lags)):
+        '''
+        flag: lags序列的下标
+        '''
+        # 为滞后列表预分配空间
         data_t_minus_tags = np.zeros(len(data) - lags[flag])
+        # 滞后序列的构造
         for index in range(lags[flag],len(data)):
             data_t_minus_tags[index-lags[flag]] = data[index]
         print(f"Lags:{lags[flag]}滞后:",data_t_minus_tags)
-        ACF = ACF_calculation(data,data[0:(len(data) - lags[flag])],data_t_minus_tags)
+        # 当前时刻序列
+        data_time = data[0:(len(data) - lags[flag])]
+        # ACF的计算
+        ACF = ACF_calculation(data,data_time,data_t_minus_tags)
         print(f"Lags:{lags[flag]}ACF:",ACF)
+    # 调用ACF进行对比验证
     plot_acf(data,lags=3)
     plot_pacf(data,lags=3)
     plt.show()
 
     # 一阶差分
+    # 为滞后列表预分配空间
     diff_data = np.zeros(len(data)-1)
+    # 滞后序列的构造
     for index in range(len(diff_data)):
         diff_data[index] = data[index+1] - data[index]
     for flag in range(len(lags)):
@@ -62,8 +80,12 @@ if __name__ == "__main__":
         for index in range(lags[flag], len(diff_data)):
             data_t_minus_tags[index - lags[flag]] = diff_data[index]
         print(f"Lags:{lags[flag]}滞后:", data_t_minus_tags)
-        ACF = ACF_calculation(diff_data,diff_data[0:(len(diff_data) - lags[flag])], data_t_minus_tags)
+        # 当前时刻序列
+        data_time = diff_data[0:(len(diff_data) - lags[flag])]
+        # ACF的计算
+        ACF = ACF_calculation(diff_data,data_time, data_t_minus_tags)
         print(f"Lags:{lags[flag]}ACF:", ACF)
+    # 调用ACF进行对比验证
     plot_acf(diff_data, lags=3)
     plot_pacf(diff_data,lags=3)
     plt.show()
--- README.md
+++ README.md
@@ -1,13 +1,31 @@
 # Prediction-model-to-Time-Sequence
 To better understand that how to use the model to predict the future value.</br>
+The flowing knowledge are what we ought to learn to use in reality problem!</br>
+preface:For a model,if you see it as a person that own the emotion at the same time,it also would be afraid of something.</br>
+Like,Fake correlation data that it is unsteady!So it's indispensable to handle the data.</br>
+But why we do that?</br>
+That's because the mode correlation is only the phenomenon of the data and what we have to get is the fluctuation of changes of the data.</br>
+为了更好的理解“利用模型对未来状态进行预测”的方式。</br>
+以下知识是我们应该学习并应用实际的东西!</br>
+前言:对于一个时间序列模型来说，如若你将其视为一个拥有情感的人，那么它也将有其害怕的东西。</br>
+比如，非平稳的虚假相关性数据等。于是对数据进行处理是必要的。</br>
+但为何要这样做呢</br>
+这是由于模型的相关仅仅只是数据本身表达出来的现象，我们要拿到的东西则是关于数据变化的波动。</br>
 Basic knowledge introduction [whether completed]:</br>
-1.Differential [ √ ]</br>
-2.Lags and Lags-Differential [ √ ]</br>
-3.ACF and PACF [ √ ]</br>
-4.stationarity vs Non-stationarity [  ]</br>
-model list [whether completed]:</br>
+基础知识介绍 [是否完成]:</br>
+Differential as a method to get rid of the correlation and the trend of the data,it's vital to learn.</br>
+差分操作作为剔除数据的相关性和趋势的方法，学会它至关重要。</br>
+1.Differential [ √ ] </br>
+2.Lags and Lags-Differential [ √ ] </br>
+3.ACF and PACF [ √ ] </br>
+4.stationarity vs Non-stationarity [  ] </br>
+如果到这里都能够理解，那恭喜你成功习得基础的处理和识别时间序列性质的能力，
+model list [whether completed]: </br>
 1.AR []</br>
 2.MA []</br>
 3.ARMA []</br>
 4.ARIMA []</br>
 5.SARIMA []</br>
+# Lastly
+It's not a guide book for somebody.</br>
+But at present it could be a nice an reference book for somebody who wanna build a model to solve the easy problem or test.</br>
